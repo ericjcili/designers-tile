d85d4a40db45faa1277c248233d343b95a93fd8e
ericjcili <ericjcili@gmai.com>
2022-01-19 00:44:02 -0500

    test

---
+++ js/read-more.js
@@ -0,0 +1,13 @@
+
+    $("#toggle").click(function() {
+      var elem = $("#toggle").text();
+      if (elem == "Read More") {
+        //Stuff to do when btn is in the read more state
+        $("#toggle").text("Read Less");
+        $("#text").slideDown();
+      } else {
+        //Stuff to do when btn is in the read less state
+        $("#toggle").text("Read More");
+        $("#text").slideUp();
+      }
+    });
